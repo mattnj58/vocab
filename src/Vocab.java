e58c75e0b64eff054af9d45577c07147333950d5
mattnj58 <mattnj58@gmail.com>
2020-10-22 21:05:13 -0400

    added more words

--- src/Vocab.java
+++ src/Vocab.java
@@ -1,24 +1,28 @@
-import java.io.*;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 public class Vocab {
 
     public static void main(String[] args) throws InterruptedException {
-        String vocab = "Dictionaries/vocab";
-//        String vocab = "Dictionaries/test8";
+//        String vocab = "Dictionaries/vocab";
+        String testNum = "12";
+        String vocab = "Dictionaries/test"+testNum;
+//        String vocab = "Dictionaries/vocab";
         HashMap<String, String> dict = createDict(vocab);
         Random rand = new Random();
         int wordNum = rand.nextInt(dict.size());
 
         System.out.println("Dictionary Size: "+dict.size());
 
-        List<String> words = new ArrayList<>(dict.keySet());
+        ArrayList<String> words = new ArrayList<>(dict.keySet());
 
         System.out.println(words.get(wordNum));
-//        TimeUnit.SECONDS.sleep(10);
-
-//        System.out.println(dict.get(words.get(wordNum)));
+        Thread.sleep(6000);
+        System.out.println(dict.get(words.get(wordNum)));
     }
 
     private static HashMap createDict(String vocab){
