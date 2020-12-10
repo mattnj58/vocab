2ad94732f382194bcdd3538fdbde9fa2f87af03c
mattnj58 <mattnj58@gmail.com>
2020-12-10 12:54:18 -0500

    added more words

--- src/Vocab.java
+++ src/Vocab.java
@@ -8,27 +8,33 @@ import java.util.Random;
 public class Vocab {
 
     public static void main(String[] args) throws InterruptedException {
-//        String vocab = "Dictionaries/vocab";
-        String testNum = "4";
+        String testMain = "Dictionaries/testMain";
+        String testNum = "Main";
         String vocab = "Dictionaries/test"+testNum;
         HashMap<String, String> dict = createDict(vocab);
         Random rand = new Random();
         int wordNum = rand.nextInt(dict.size());
 
-        System.out.println("Test #" + testNum);
+        System.out.println("Test: " + testNum);
+        System.out.println("Test Size: " + dict.size());
 
         ArrayList<String> words = new ArrayList<>(dict.keySet());
 
         System.out.println(words.get(wordNum));
-        Thread.sleep(10000);
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println((dict.get(words.get(wordNum))));
+            System.exit(0);
+        }
         System.out.println(dict.get(words.get(wordNum)));
     }
 
     private static HashMap createDict(String vocab){
         BufferedReader reader;
+        HashMap<String, String > dict = new HashMap<>();
         String line1;
         String line2;
-        HashMap<String, String > dict = new HashMap<>();
 
         try {
             reader = new BufferedReader(new FileReader(vocab));
