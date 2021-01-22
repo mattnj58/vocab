b831c09e5470d1ddf145eea42927e06b0e86026b
mattnj58 <mattnj58@gmail.com>
2021-01-22 13:28:55 -0500

    more words

--- src/Vocab.java
+++ src/Vocab.java
@@ -9,7 +9,7 @@ public class Vocab {
 
     public static void main(String[] args) throws InterruptedException {
         String testMain = "Dictionaries/testMain";
-        String testNum = "18";
+        String testNum = "19";
         String vocab = "Dictionaries/test"+testNum;
         HashMap<String, String> dict = createDict(vocab);
         Random rand = new Random();
